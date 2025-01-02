d79e6201e9dcfd38635673ba2f5fa5ab6741bf4d
Thirishaa Swamynathan <95125580+Thirishaa@users.noreply.github.com>
2025-01-02 16:02:32 +0530

    Create Book.java

---
+++ src/main/java/Book.java
@@ -0,0 +1,46 @@
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void displayBookDetails() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: $" + price);
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 10.99);
+        book1.displayBookDetails();
+    }
+}
